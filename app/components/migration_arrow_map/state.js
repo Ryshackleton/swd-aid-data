2cfd8e38d74e0e5746a6f7d18c6bc2436aa5d3dc
Ryan Shackleton <ryshack@uw.edu>
2019-03-05 02:37:15 -0800

    Add lots of new state to support arrows

--- app/components/migration_arrow_map/state.js
+++ app/components/migration_arrow_map/state.js
@@ -1,15 +1,27 @@
-export const state = {
+export default {
   // chart view states
   isCartogram: false,
   isDisplayingVoronoi: true,
+  isDisplayingArrows: true,
+
+  bubbleDefaultOpacity: 0.9,
+  bubbleHiddenOpacity: 0.15,
 
   // color data array
   colorRadiusData: [], // pick from props
   geographyPropName: 'state',
+  colorPropName: 'Percent Change',
 
   // flow data array
   flowData: [], // pick from props
   arrowOriginPropName: 'Origin',
   arrowDestinationPropName: 'Destination',
-  colorPropName: 'Percent Change',
+  arrowDefaultOpacity: 0.075,
+  arrowFlowPropName: 'Total',
+  arrowHighlightOpacity: 0.85,
+  arrowMidPointWeight: 0.4, // controls curvature of arrows 0 straight, 1 curved
+  arrowScaleRangePixels: [10, 40],
+  arrowScaleColorRange: ['lightgrey', '#333333'],
+  destinationArrowPadding: 0.75,
+  isFlowingOriginToDestination: true,
 };
